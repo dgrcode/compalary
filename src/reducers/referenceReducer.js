051d02272446fe74b2bf5b7a0a86f8dbfbbf5255
Daniel Gonzalez Reina <dangonrei@gmail.com>
2018-10-21 20:09:57 +0100

    Fix regex to match any non digit

--- src/reducers/referenceReducer.js
+++ src/reducers/referenceReducer.js
@@ -6,7 +6,7 @@ const defaultState = {
 const referenceReducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'CHANGE_REFERENCE_SALARY':
-      const nextSalary = Number.parseFloat(action.payload.nextSalary.replace(/,/g, ''))
+      const nextSalary = Number.parseFloat(action.payload.nextSalary.replace(/\D/g, ''))
 
       return {
         ...state,
